7749bc9cc0d28f9fd2186a931509a83d6e97be4a
RohanW-star <rohanwernars@gmail.com>
2026-03-23 12:35:33 +0100

    Added more of the art that was send in the discord and made some changes to start making the prototype

--- Assets/Scripts/ButtonChangeScore.cs
+++ Assets/Scripts/ButtonChangeScore.cs
@@ -6,12 +6,22 @@ public class ButtonChangeScore : MonoBehaviour
     public int changeB;
     public int changeC;
     public GameObject changedBuilding;
+    public GameObject oldBuilding;
     public GameObject[] extraChanges;
 
     public void ApplyEffect()
     {
         ScoresManager.Instance.ChangeScores(changeA, changeB, changeC);
         EventManager.Instance.EventDeactivated();
+
+        Destroy(oldBuilding);
+        Instantiate(changedBuilding);
+
+        foreach (var change in extraChanges)
+        {
+            change.SetActive(true);
+        }
+
         Destroy(transform.root.gameObject);
     }
 }
