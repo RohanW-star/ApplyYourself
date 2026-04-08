59da148cbd453546b1c73a73fd1e3664531c5e03
RohanW-star <rohanwernars@gmail.com>
2026-04-08 16:28:53 +0200

    font and lighting in prototype scene

--- Assets/Scripts/BuildingEvents.cs
+++ Assets/Scripts/BuildingEvents.cs
@@ -5,15 +5,19 @@ public class BuildingEvents : MonoBehaviour
 {
     public Canvas canvas;
     private ClickOnBuildings ClickOnBuildings;
+    private AudioSource audioSource;
 
     private void Start()
     {
         ClickOnBuildings = Object.FindFirstObjectByType<ClickOnBuildings>();
+        GameObject obj = GameObject.FindGameObjectWithTag("Audio");
+        audioSource = obj.GetComponent<AudioSource>();
     }
     public void SpawnCanvas()
     {
         Instantiate(canvas, canvas.transform.position, Quaternion.identity);
         ClickOnBuildings.enabled = false;
+        audioSource.PlayOneShot(audioSource.clip);
         Destroy(this.gameObject);
     }
 }
